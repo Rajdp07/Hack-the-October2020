eb33da170f43713251ecdf1e996666744ed65913
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2020-10-15 13:36:54 +0530

    Add files via upload

---
+++ Aman1905/Hello_World.java
@@ -0,0 +1,12 @@
+package hello_world;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello WOrld");
+
+	}
+
+}
